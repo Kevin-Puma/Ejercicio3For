733ed8874cb8c7b98ed97cbc4b7ef68f76798bd2
User <User@DESKTOP-CRJD420>
2023-09-21 23:40:22 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio3For/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Ingrese el numero de notas: ");
+int cantidadNotas = int.Parse(Console.ReadLine());
+
+int sumaNotas = 0;
+
+for (int i = 1; i <= cantidadNotas; i++)
+{
+Console.Write("Ingrese la nota "+i+":");
+int nota = int.Parse(Console.ReadLine());
+sumaNotas += nota;
+}
+
+int promedio = sumaNotas / cantidadNotas;
+
+Console.WriteLine("El promedio es: " +promedio+ "");
+
+Console.ReadKey();
+  
